005ff0702be0dead615d38c43606f9870d800963
Tomas Perez <tomaspro56@gmail.com>
2020-11-21 19:32:38 -0500

    Isolation Training P1

--- frontend/src/Utils/Mocks/Rutinas.js
+++
@@ -1,9 +0,0 @@
-export const Rutinas = [
-    {
-        titulo: "Abdomen",
-        valor: 0,
-        info: "Esta rutina contará con varios ejercicios que ayudarán a fortalecer su abdomen. Recomendamos tomar un descanso después de cada sesión y tomar agua.",
-        contenido: "<div><h3>Abdominales</h3><p>3 series de 5 repeticiones cada una</p><img class='tamaño' src='/images/Abdo.gif'/></div><div><h3>Crunch invertido</h3><p>3 series de 12 repeticiones cada una</p><img class='tamaño' src='/images/Crunch.png'/></div><div><h3>Crunch Clasico</h3><p>3 series de 10 repeticiones cada una</p><img class='tamaño' src='/images/CrunchC.jpg'/></div>",
-        fondo: "/images/Abdomen.jpg"
-    }
-]
